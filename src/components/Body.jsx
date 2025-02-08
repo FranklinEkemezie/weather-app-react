6d934b58dc7bc0e7e15d8e6adf3e03d56af00546
Shadow Walker <franklynpeter2006@gmail.com>
2025-02-08 07:06:39 +0100

    update Body component to change the background when a new weather is loaded

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,20 +1,63 @@
 import defaultBgImg from "../assets/images/1688532648369.jpg";
 import {useContext, useEffect, useState} from "react";
 import AppContext from "../contexts/AppContext.jsx";
-import env from "../../env.js";
+import fetchWeatherData from "../functions/fetchWeatherData.js";
+import parseWeatherData from "../functions/parseWeatherData.js";
 
 // eslint-disable-next-line react/prop-types
 function Body( { children } ) {
 
-    const { isLoaded, weather } = useContext(AppContext);
+    const {
+        isLoaded, setIsLoaded,
+        city, setCity,
+        weather, setWeather
+    } = useContext(AppContext);
 
-    const [bgImgUrl, setBgImg] = useState(defaultBgImg)
+    const [bgImgUrl, setBgImg] = useState(defaultBgImg);
 
+    // Little helper function to organise api URL and api Key
+    const buildApiDetails = (key, url) => ({key, url});
+
+    const {
+        VITE_OPEN_WEATHER_API_BASE_URL, VITE_OPEN_WEATHER_API_KEY,
+        VITE_UNSPLASH_API_BASE_URL, VITE_UNSPLASH_API_SECRET_KEY
+    } = import.meta.env;
+
+    const openWeatherApiDetails = buildApiDetails(VITE_OPEN_WEATHER_API_KEY, VITE_OPEN_WEATHER_API_BASE_URL);
+    const unsplashImgApiDetails = buildApiDetails(VITE_UNSPLASH_API_SECRET_KEY, VITE_UNSPLASH_API_BASE_URL);
+
+    // Fetch and load weather data on first render
     useEffect(() => {
 
-        if (isLoaded) {
-            const unsplashImgApiUrl = `https://api.unsplash.com/photos/random?query=weather-${weather.description}&client_id=${env.API.UNSPLASH.ACCESS_KEY}&orientation=landscape&w=1920&h=1080`;
+        const { key, url } = openWeatherApiDetails;
 
+        setIsLoaded(false);
+
+        fetchWeatherData(city, key, url)
+            .then(data => {
+
+                const parsedWeatherData = parseWeatherData(data);
+
+                setWeather(parsedWeatherData);
+                setCity(city);
+                setIsLoaded(true);
+
+                return parsedWeatherData
+            })
+        ;
+
+    }, []);
+
+
+    // Load the Background Img from Unsplash once the image loads
+    useEffect(() => {
+
+        const { key, url } = unsplashImgApiDetails;
+        const { description } = weather;
+
+        const unsplashImgApiUrl = `${url}?query=weather-${description}&client_id=${key}&orientation=landscape&w=1920&h=1080`;
+
+        if (isLoaded) {
             fetch(unsplashImgApiUrl)
                 .then(response => {
                     if (! response.ok) {
@@ -24,9 +67,6 @@ function Body( { children } ) {
                     return response.json();
                 })
                 .then(data => {
-
-                    console.log(data);
-
                     setBgImg(data.urls.full);
                 })
             ;
